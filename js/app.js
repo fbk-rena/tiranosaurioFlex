30578173abe2773ada0a7f2b9318245d25df2dfb
fbk-rena <fbk.rena@gmail.com>
2017-05-09 16:54:15 -0500

    Ocultar texto funcionando

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
 //alert("Hola");
 var tache = document.getElementsByTagName("span");
 
-for(var i = 0; i < tache.length; i++) {
-    tache[i].addEventListener('click', function(){
-        this.parentNode.style.display='none';
+for (var i = 0; i < tache.length; i++) {
+    tache[i].addEventListener('click', function () {
+        this.parentNode.style.display = 'none';
     });
 }
+
+var textExtint = document.getElementById("text-extincion");
+var btnOrigen = document.getElementById("origen");
+btnOrigen.addEventListener('mouseover', function() {
+    textExtint.parentNode.style.display= 'none';
+});
+
+var textOrigen = document.getElementById("text-origen");
+var btnExtint = document.getElementById("extincion");
+btnExtint.addEventListener('mouseover', function() {
+    textOrigen.parentNode.style.display= 'none';
+});
+
+
